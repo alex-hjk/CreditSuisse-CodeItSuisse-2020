6ab4f10212a6ced7b1ffeda512fec91dafdb77e3
Lim Yee Han <limyeehan@gmail.com>
2020-09-26 17:58:55 +0800

    start clean_floor

---
+++ codeitsuisse/routes/cleanfloor.py
@@ -0,0 +1,30 @@
+import logging
+import json
+import sys
+
+from flask import request, jsonify;
+
+from codeitsuisse import app;
+
+logger = logging.getLogger(__name__)
+
+
+# print(number_of_salads, file=sys.stdout)
+# print(salad_prices_street_map, file=sys.stdout)
+
+@app.route('/clean_floor', methods=['POST'])
+def clean_floor():
+	# Parsing input
+	byte_value = request.data
+
+	# Decode UTF-8 bytes to Unicode, and convert single quotes
+	# to double quotes to make it valid JSON
+	my_json = byte_value.decode('utf8').replace("'", '"')
+
+	# Load the JSON to a Python list & dump it back out as formatted JSON
+	data = json.loads(my_json)
+
+	print(data)
+	print(type(data))
+
+	return json.dumps(data)
