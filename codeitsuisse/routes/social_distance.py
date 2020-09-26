8ff67d84337421209e5b8f12389c100cf95a1cac
Lim Yee Han <limyeehan@gmail.com>
2020-09-26 22:19:04 +0800

    SD 1

--- codeitsuisse/routes/social_distance.py
+++ codeitsuisse/routes/social_distance.py
@@ -18,9 +18,36 @@ def social_distancing():
 	# Parsing input
 	data = request.get_json();
 
-	pp.pprint(data)
+	test_dict = data['tests']
 
-	print(data)
-	print(type(data))
+	output = {"answers": {}}
 
-	return json.dumps(data)
+	for key, val in test_dict.items():
+		seats = val["seats"]
+		people = val["people"]
+		spaces = val["spaces"]
+
+		output["answers"][key] = SD(seats, people, spaces)
+
+	return json.dumps(output)
+
+
+def SD(seats, people, spaces):
+	print(f"input: {seats, people, spaces}")
+
+	# Base case - single person left
+	if people == 1 and seats:
+		return seats
+
+	# Impossible case - not enough seats to accomodate people and spaces
+	if seats < people + spaces:
+		return 0
+
+	# When you fill, have to pad spaces
+	fill = SD(seats - spaces - 1, people - 1, spaces)
+	print(f"  from fill {fill}")
+
+	no_fill = SD(seats - 1, people, spaces)
+	print(f"  from no_fill {no_fill}")
+
+	return fill + no_fill
