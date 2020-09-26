281d58f166ef05adc3ca1f1a7489a05920eda6f4
Lim Yee Han <limyeehan@gmail.com>
2020-09-26 21:10:12 +0800

    started contact trace

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -8,5 +8,6 @@ import codeitsuisse.routes.intelligent_farming
 import codeitsuisse.routes.fruitbasket
 import codeitsuisse.routes.olympiad_of_babylon
 import codeitsuisse.routes.inventory_management
+import codeitsuisse.routes.contractTrace
 
 
