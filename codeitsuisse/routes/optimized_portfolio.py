381c9126e8e1099a20d524847ad103540b7628b5
Alexander Ho <alex.hjk@gmail.com>
2020-09-27 10:00:35 +0800

    pf

--- codeitsuisse/routes/optimized_portfolio.py
+++ codeitsuisse/routes/optimized_portfolio.py
@@ -23,15 +23,19 @@ def optimized_portfolio():
 	testCases = req["inputs"] #testCases is a list of dicts
 	for test in testCases:
 		pfInfo = test["Portfolio"]
+		print(pfInfo)
 		idxInfo = test["IndexFutures"]
+		print(idxInfo)
 		res["outputs"].append(getOptimized(pfInfo,idxInfo))
 	return json.dumps(res)
 
 def getOptimized(pfDict, idxList):
 	result = sorted(idxList, key = lambda x: (x["CoRelationCoefficient"]/x["FuturePrcVol"], 1/(x["IndexFuturePrice"]*x["Notional"])))
+	print(result[0]["Name"])
 	optRatio = round(result[0]["CoRelationCoefficient"]*pfDict["SpotPrcVol"]/result[0]["FuturePrcVol"],3)
 	print(optRatio)
 	numFtrs = round(optRatio*pfDict["Value"]/(result[0]["IndexFuturePrice"]*result[0]["Notional"]))
+	print(numFtrs)
 
 	return {
 		"HedgePositionName": result[0]["Name"],
