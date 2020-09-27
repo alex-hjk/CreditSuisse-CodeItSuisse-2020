22f166c06aced04047691bceb27a90215a4af5b3
Alexander Ho <alex.hjk@gmail.com>
2020-09-27 12:05:01 +0800

    sprmkt

--- codeitsuisse/routes/supermarket.py
+++ codeitsuisse/routes/supermarket.py
@@ -24,19 +24,22 @@ def evaluate():
 
 
 def numSteps(maze, start, end):
+    start = [start[1],start[0]]
+    end = [end[1],end[0]]
     q = Queue()
     q.put([start,1])
     while q.empty()==False:
         curr = q.get()
-        maze[curr[0][1]][curr[0][0]]==1
+        print(curr)
+        maze[curr[0][0]][curr[0][1]]=1
         if curr[0]==end:
             return curr[1]
-        if curr[0][0]>0 and maze[curr[0][1]][curr[0][0]-1]!=1:
+        if curr[0][0]>0 and maze[curr[0][0]-1][curr[0][1]]!=1:
             q.put([[curr[0][0]-1,curr[0][1]],curr[1]+1])
-        if curr[0][0]<len(maze[0])-1 and maze[curr[0][1]][curr[0][0]+1]!=1:
+        if curr[0][0]<len(maze)-1 and maze[curr[0][0]+1][curr[0][1]]!=1:
             q.put([[curr[0][0]+1,curr[0][1]],curr[1]+1])
-        if curr[0][1]>0 and maze[curr[0][1]-1][curr[0][0]]!=1:
+        if curr[0][1]>0 and maze[curr[0][0]][curr[0][1]-1]!=1:
             q.put([[curr[0][0],curr[0][1]-1],curr[1]+1])
-        if curr[0][1]<len(maze)-1 and maze[curr[0][1]+1][curr[0][0]]!=1:
+        if curr[0][1]<len(maze[0])-1 and maze[curr[0][0]][curr[0][1]+1]!=1:
             q.put([[curr[0][0],curr[0][1]+1],curr[1]+1]) 
     return -1
