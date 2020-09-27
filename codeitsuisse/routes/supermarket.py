b80b6e139c5cc8f84e820a5f416bf6df9b689ee4
Alexander Ho <alex.hjk@gmail.com>
2020-09-27 11:34:30 +0800

    sprmkt

--- codeitsuisse/routes/supermarket.py
+++ codeitsuisse/routes/supermarket.py
@@ -6,17 +6,37 @@ from flask import request, jsonify;
 
 from codeitsuisse import app;
 
+from queue import Queue
+
 logger = logging.getLogger(__name__)
 
 @app.route('/supermarket', methods=['POST'])
 def evaluate():
-    data = request.get_json();
-    print(data, file=sys.stdout)
-    logging.info("data sent for evaluation {}".format(data))
-    inputValue = data.get("input");
-    result = inputValue * inputValue
-    logging.info("My result :{}".format(result))
-    return json.dumps(result);
+    req = request.get_json()
+    res = {
+        "answers": {}
+    }
+    testCases = req["tests"]
+    for test in testCases:
+        res["answers"][test] = numSteps(test["maze"],test["start"],test["end"])
+    
+    return json.dumps(res)
 
 
-
+def numSteps(maze, start, end):
+    q = Queue()
+    q.put([start,1])
+    while q.empty()==False:
+        curr = q.get()
+        maze[curr[0][0]][curr[0][1]]==1
+        if curr[0]==end:
+            return curr[1]
+        if curr[0][0]>0 and maze[curr[0][1]][curr[0][0]-1]!=1:
+            q.put([[curr[0][0]-1,curr[0][1]],curr[1]+1])
+        if curr[0][0]<len(maze[0])-1 and maze[curr[0][1]][curr[0][0]+1]!=1:
+            q.put([[curr[0][0]+1,curr[0][1]],curr[1]+1])
+        if curr[0][1]>0 and maze[curr[0][1]-1][curr[0][0]]!=1:
+            q.put([[curr[0][0],curr[0][1]-1],curr[1]+1])
+        if curr[0][1]<len(maze)-1 and maze[curr[0][1]+1][curr[0][0]]!=1:
+            q.put([[curr[0][0],curr[0][1]+1],curr[1]+1]) 
+    return -1
